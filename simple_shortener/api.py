c2a3b3f8b4c2825121373ae0de105244d573fd68
Martin Zlocha <mz4315@ic.ac.uk>
2018-10-30 10:30:46 +0000

    Implement simple redirect to a dummy url.

--- simple_shortener/api.py
+++ simple_shortener/api.py
@@ -1,21 +1,31 @@
 from flask_restful import Api, Resource, reqparse
+from flask import redirect
 
 from simple_shortener import app
 
 api = Api(app)
 
 
-parser = reqparse.RequestParser()
-parser.add_argument('url', required=True, help='Missing Url to shorten')
-
-
 class ShortenUrl(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('url', required=True, help='Missing Url to shorten')
+
     def post(self):
-        args = parser.parse_args(strict=True)
+        args = self.parser.parse_args(strict=True)
 
         # ToDo
 
         return {'shortened_url': 'ToDo'}, 201
 
 
+class Redirect(Resource):
+    def get(self, alias):
+
+        # ToDo
+
+        return redirect("https://www.google.com/", code=301)
+
+
 api.add_resource(ShortenUrl, '/shorten_url')
+api.add_resource(Redirect, '/<alias>')
--- tests/test_api.py
+++ tests/test_api.py
@@ -12,13 +12,13 @@ class APITestCase(unittest.TestCase):
         response = self.app.post('/shorten_url', data={'url': 'google.com'})
         self.assertEqual(response.status_code, 201)
 
-    def test_get_is_invalid(self):
-        response = self.app.get('/shorten_url')
-        self.assertEqual(response.status_code, 405)
-
     def test_empty_post_fails(self):
         response = self.app.post('/shorten_url')
         self.assertEqual(response.status_code, 400)
+
+    def test_redirect(self):
+        response = self.app.get('/test')
+        self.assertEqual(response.status_code, 301)
 
 
 if __name__ == '__main__':
